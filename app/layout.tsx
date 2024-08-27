5753fc750435ea09c3cb0eaa1e97ea2a2368655b
vivilu <viviana.recalde@gmail.com>
2024-08-27 16:06:23 -0500

    final of tutorial

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,14 @@
 import '@/app/ui/global.css';
 import { roboto } from '@/app/ui/fonts';
 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 export default function RootLayout({
   children,
 }: {
